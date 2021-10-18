598f3c625dad6ed13da869d6e62598735a645234
ruggerygusmao <ruggery402@gmail.com>
2021-10-17 21:14:58 -0300

    atividade tela-cadastro

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cadastro from './Cadastro/Cadastro';
+
+function App() {
+  return (
+    <div className="App">
+      <Cadastro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cadastro/Cadastro.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from '../logo.svg';
+import "./Cadastro.css"
+
+export default function Cadastro(){
+
+    return (
+        <>
+            <div className="container">
+                <div className="item1">
+                        <div className="titulo">   
+                            <h3>Cadastro</h3>
+                            <p>Preencha os dados abaixo para começar.</p>
+                        </div>
+
+                        <input type="text" placeholder="Nome" />
+                        <input type="text" placeholder="Sobrenome" />
+                        <input type="e-mail" placeholder="E-mail" />
+                        <input type="password" placeholder="Senha" />
+                  
+                        <button className="botao">Concluir cadastro</button>
+                </div>
+
+                <div className="item2">
+                    <img src={logo} alt="logo.png"></img>
+                    <h1>App React</h1>
+                </div>      
+            </div>
+        </>
+    );
+}
